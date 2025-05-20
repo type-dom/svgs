828d9dbead2a2a2b7fe9f69af57ff85cbd9ee92e
xjf7711 <xjf7711@qq.com>
2025-05-20 21:38:12 +0800

    chore: add gh-pages

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/common-index';
-export * from './lib/element-plus-index';
-export * from './lib/fluentui-index';
-export * from './lib/other/index';
-export * from './lib/icon';
--- tests/404.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { Td404Svg } from '../src/lib/common/404';
-
-describe('Td404Svg', () => {
-  it('should work', () => {
-    const svg404 = new Td404Svg();
-    expect(svg404.className).toEqual('Td404Svg');
-  });
-});
